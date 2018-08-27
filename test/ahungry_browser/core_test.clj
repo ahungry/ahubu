d08a5a8ea40f5eb75faa769878199a6c37652be6
Matthew Carter <m@ahungry.com>
2018-08-26 22:54:39 -0400

    init

---
+++ test/ahungry_browser/core_test.clj
@@ -0,0 +1,7 @@
+(ns ahungry-browser.core-test
+  (:require [clojure.test :refer :all]
+            [ahungry-browser.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

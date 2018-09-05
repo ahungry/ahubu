1f5bdd6b472d43dc9075cd4b587b0acfdc9f8ba3
Matthew Carter <m@ahungry.com>
2018-09-05 00:06:34 -0400

    Remove annoying divs/classes

--- src/ahubu/lib.clj
+++ src/ahubu/lib.clj
@@ -774,6 +774,32 @@
             )
         (range (.getLength els)))))
 
+(defn remove-annoying-div [dom id]
+  (let [el (-> dom (.getElementById id))]
+    (when el (.remove el))))
+
+(defn remove-annoying-divs [dom]
+  (let [ids (str/split (slurp "conf/dom-id-ignores.txt") #"\n")]
+    (doall
+     (map #(remove-annoying-div dom %) ids))))
+
+(defn remove-annoying-class [dom class-name]
+  (println class-name)
+  (let [els (-> dom (.getElementsByClassName class-name))]
+    (doall
+     (map
+      (fn [_]
+        ;; We remove item 0, because each remove causes a reindex
+        (let [el (-> els (.item 0))]
+          (-> el .remove)))
+      (range (.getLength els))))))
+
+(defn remove-annoying-classes [dom]
+  (let [ids (str/split (slurp "conf/dom-class-ignores.txt") #"\n")]
+    (println ids)
+    (doall
+     (map #(remove-annoying-class dom %) ids))))
+
 (defn new-scene []
   (run-later
    (let [
@@ -832,6 +858,8 @@
                     (set-omnibar-text-to-url)
 
                     ;; map over all the page links on load
+                    (-> webengine .getDocument remove-annoying-divs)
+                    (-> webengine .getDocument remove-annoying-classes)
                     (-> webengine .getDocument (.getElementsByTagName "a") el-link-fn)
                     (-> webengine (.setUserAgent "Mozilla/5.0 (Windows NT 6.1) Gecko/20100101 Firefox/61.0"))
 
